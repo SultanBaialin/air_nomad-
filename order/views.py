3d205677698b307caf5c42bd7f3aa329a7e4348f
Dolya26 <doolat.daiyrkulov@gmail.com>
2023-02-09 20:00:50 +0600

    added order1

--- order/views.py
+++ order/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from rest_framework.generics import CreateAPIView, ListCreateAPIView
+# from order.serializers import OrderSerializer
+# from rest_framework.response import Response
+#
+#
+# class CreatedOrderView(ListCreateAPIView):
+#     serializer_class = OrderSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         user = request.user
+#         orders = user.orders.all()
+#         serializer = OrderSerializer(orders, many=True)
+#         return Response(serializer.data, status=200)
