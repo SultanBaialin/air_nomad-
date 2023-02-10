8969f66fa95a83d59b863c479470abdc8d4d5969
SultanBaialin <baialinovsultan@gmail.com>
2023-02-10 19:10:11 +0600

    10 feb

--- product/models.py
+++ product/models.py
@@ -1,4 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from category.models import Category
+from ckeditor.fields import RichTextField
 
-# from django.db import models
-#
-# # Create your models here.
+User = get_user_model()
+
+
+class Product(models.Model):
+    STATUS_CHOISES=(
+        ('in_stock', 'в наличии'),
+        ('out_of_stock', 'нет в наличии')
+    )
+
+    owner = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='products')
+    title = models.CharField(max_length=150)
+    description = RichTextField()
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.RESTRICT)
+    image = models.ImageField(upload_to='images')
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    stock = models.CharField(choices=STATUS_CHOISES, max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
