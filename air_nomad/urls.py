98255e4db7b67ddcdf9d225d3795f40dc6e2209f
AkopianEvgenii112708 <eakopian2002@mail.ru>
2023-02-10 19:06:58 +0600

    Category final

--- air_nomad/urls.py
+++ air_nomad/urls.py
@@ -18,8 +18,17 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.routers import SimpleRouter
 
 ...
+
+from category.views import CategoryViewSet
+
+
+router = SimpleRouter()
+router.register('categories', CategoryViewSet)
+
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -40,6 +49,7 @@ urlpatterns = [
    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
    path('admin/', admin.site.urls),
    path('api/v1/accounts/', include('account.urls')),
+   path('api/v1/', include(router.urls)),
 
 ]
 
