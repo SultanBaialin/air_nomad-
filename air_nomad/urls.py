67a7e0b87fbaa9361c69034ac96fa573af5ee02d
tokami3d <dzholdoshev1@gmail.com>
2023-02-11 17:59:29 +0600

    added media

--- air_nomad/urls.py
+++ air_nomad/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.routers import SimpleRouter
+from django.conf import settings
 
 ...
 
@@ -54,3 +55,4 @@ urlpatterns = [
 
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
