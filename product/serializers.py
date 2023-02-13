a978beddaaf6f3af912b3d28aa3470bd91fe756b
SultanBaialin <baialinovsultan@gmail.com>
2023-02-13 19:33:49 +0600

    13.02.23

--- product/serializers.py
+++ product/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, ProductImages
+
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImages
+        fields = '__all__'
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -18,3 +24,29 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        product = Product.objects.create(**validated_data)
+        images_data = request.FILES.getlist('images')
+        for image in images_data:
+            ProductImages.objects.create(image=image, product=product)
+        return product
+
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+        for k, v in validated_data.items():
+            setattr(instance, k, v)
+        instance.save()
+        instance.images.all().delete()
+        images_data = request.FILES.getlist('images')
+        for image in images_data:
+            ProductImages.objects.create(image=image, product=instance)
+        return instance
+
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
+        repr['images'] = ProductImageSerializer(instance.images.all(), many=True).data
+        # repr['reviews'] = ReviewCreateSerializer(instance.reviews.all(), many=True).data
+
+        return repr
