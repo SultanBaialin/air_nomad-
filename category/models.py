939ec61075b9dff942e3bac9f1f693e17b7f5908
SultanBaialin <baialinovsultan@gmail.com>
2023-02-14 20:46:49 +0600

    category final1

--- category/models.py
+++ category/models.py
@@ -8,23 +8,25 @@ from django.dispatch import receiver
 from django.utils.text import slugify
 
 
-# Create your models here.
+class CountryCategory(models.Model):
+    slug = models.SlugField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
 
 class Category(models.Model):
     slug = models.SlugField(max_length=50, primary_key=True)
     name = models.CharField(max_length=50, unique=True)
 
-    # def save(self, *args, **kwargs):
-    #     self.slug = slugify(self.name)
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
-
-
-class CountryCategory(Category):
-    slug1 = models.SlugField(max_length=50, primary_key=True)
-    name1 = models.CharField(max_length=50, unique=True)
 
 
 @receiver(pre_save, sender=Category)
